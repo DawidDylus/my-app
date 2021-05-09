121858fc472c4c891b5d0c1980c1beffe5cfa615
Dawid Dylus <36481644+DawidDylus@users.noreply.github.com>
2021-05-09 12:35:19 +0200

    Creating Navigation Bar. Installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import other components we made
+import {Home} from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import {Navigation} from './Navigation';
+
+// import modules to convigure route settings
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
+
+    <BrowserRouter>
     <div className="container">
       <h3 className="m-3 d-flex justify-content-center" >
         React
       </h3>     
-    </div>
+      
+      <Navigation/>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/department" component={Department} />
+        <Route path="/employee" component={Employee} />        
+      </Switch>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
